0b6a8888299c667486b4a6306fcdc80cb0ae941f
Andrey Sushkov <>
2022-04-21 15:47:45 +0300

    Initial commit

---
+++ MyApp/Modules/MainVC.swift
@@ -0,0 +1,21 @@
+//
+//  MainVC.swift
+//  MyApp
+//
+//  Created by Andrey Sushkov on 21.04.22.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+   
+
+}
