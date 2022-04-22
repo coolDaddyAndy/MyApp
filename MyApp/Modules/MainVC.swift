28c8192d934ea8c5c3b6dac1b2a5eb01bb885e80
Andrey Sushkov <>
2022-04-22 12:05:41 +0300

    Extensions for delegates added

--- MyApp/Modules/MainVC.swift
+++ MyApp/Modules/MainVC.swift
@@ -6,20 +6,76 @@
 //
 
 import UIKit
+import CoreLocation
 
 class MainVC: UIViewController {
     @IBOutlet weak var cityLabel: UILabel!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var searchTextField: UITextField!
     
-
+    var weatherManager = WeatherManager()
+    var locationManager = CLLocationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.requestLocation()
+        weatherManager.delegate = self
+        searchTextField.delegate = self
+    }
+
+}
+
+extension MainVC: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        searchTextField.endEditing(true)
     }
     
+    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
+        if textField.text != "" {
+            return true
+        } else {
+            textField.placeholder = "Give city name, please"
+            return false
+        }
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if let city = searchTextField.text {
+            weatherManager.getWeatherViaCity(city: city)
+        }
+        searchTextField.text = ""
+    }
+}
 
-   
+extension MainVC: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.last {
+            locationManager.stopUpdatingLocation()
+            let latitude = location.coordinate.latitude
+            let longitude = location.coordinate.longitude
+            weatherManager.getWeatherViaLatAndLon(latitude: latitude, longitude: longitude)
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print ("Something wrong with location manager")
+    }
+}
 
+extension MainVC: WeatherDataDelegate {
+    func updateWeather(_ weatherManager: WeatherManager, weather: WeatherModel) {
+        DispatchQueue.main.async {
+            self.temperatureLabel.text = weather.temperatureString
+            self.cityLabel.text = weather.cityName
+            self.conditionImageView.image = UIImage(systemName: weather.conditionName)
+        }
+    }
+    
+    func errorRecieved(_ error: Error) {
+        print ("Error while updating UI: \(error)")
+    }
 }
